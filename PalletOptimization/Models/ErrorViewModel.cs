9698dcd6770b7bcaf80fe0bf27c6f011992bf0f1
godeepsheep <godeepsheep@protonmail.com>
2024-11-11 11:51:44 +0100

    First Commit
    sdfds

---
+++ PalletOptimization/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PalletOptimization.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
