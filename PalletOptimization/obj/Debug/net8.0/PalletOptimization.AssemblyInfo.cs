f8baedc55184b450a59076d97eb9314be67533d7
godeepsheep <godeepsheep@protonmail.com>
2024-11-15 12:12:08 +0100

    esf
    esf

--- PalletOptimization/obj/Debug/net8.0/PalletOptimization.AssemblyInfo.cs
+++ PalletOptimization/obj/Debug/net8.0/PalletOptimization.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PalletOptimization")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9698dcd6770b7bcaf80fe0bf27c6f011992bf0f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed9890c7cad918507c0c0c42fffdc597971d4fa2")]
 [assembly: System.Reflection.AssemblyProductAttribute("PalletOptimization")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PalletOptimization")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
