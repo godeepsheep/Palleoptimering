731bf79cbe81c5ddfcdfb69582bda030219bcac5
godeepsheep <godeepsheep@protonmail.com>
2024-11-18 10:36:14 +0100

    gsdg
    sdgsdg

--- PalletOptimization/obj/Debug/net8.0/PalletOptimization.AssemblyInfo.cs
+++ PalletOptimization/obj/Debug/net8.0/PalletOptimization.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PalletOptimization")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8057ae4b33bf10aaf507c51d4078418e0b63916")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24d43bd7bac7385c10657bf83a345701f790fb87")]
 [assembly: System.Reflection.AssemblyProductAttribute("PalletOptimization")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PalletOptimization")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
