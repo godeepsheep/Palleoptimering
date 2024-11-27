c98a8f8a874d5d691ed12fdcbe55b3ef73a348ae
EpicoModeo <Mark-Nielsen@outlook.com>
2024-11-27 17:26:56 +0100

    stable

--- PalletOptimization/obj/Debug/net8.0/PalletOptimization.AssemblyInfo.cs
+++ PalletOptimization/obj/Debug/net8.0/PalletOptimization.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PalletOptimization")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+476dc387e6bf6fba9f188ede68cc78a47235cdd8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da10a8631060717a3b69ce42910b9db2eae658b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PalletOptimization")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PalletOptimization")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
