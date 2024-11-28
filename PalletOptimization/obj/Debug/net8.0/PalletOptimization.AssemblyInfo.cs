d1dc88fa71b666bfd5239d70a1f95fb12ef50e32
godeepsheep <godeepsheep@protonmail.com>
2024-11-28 12:17:18 +0100

    awdawd
    wadawd

--- PalletOptimization/obj/Debug/net8.0/PalletOptimization.AssemblyInfo.cs
+++ PalletOptimization/obj/Debug/net8.0/PalletOptimization.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PalletOptimization")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5f697733c31b5c74e710291c94a99f9c8fe3cf9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12ea34a37cc409a5c694efbdf6dabc53c876e8db")]
 [assembly: System.Reflection.AssemblyProductAttribute("PalletOptimization")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PalletOptimization")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
